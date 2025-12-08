6b06e216718d08c1d6485854e3b281fdbe2d9e34
Eugene Kim <simplyeugene94@gmail.com>
2025-12-07 20:41:17 -0500

    feat: Implement email OTP authentication and add a Supabase callback route.

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -1,22 +1,43 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 
-export async function login(previousState: any, formData: FormData) {
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+
+export async function sendOtp(previousState: any, formData: FormData) {
   const supabase = await createClient()
 
   const email = formData.get('email') as string
-  const password = formData.get('password') as string
 
-  const { error } = await supabase.auth.signInWithPassword({
+  const { error } = await supabase.auth.signInWithOtp({
     email,
-    password,
+    options: {
+      shouldCreateUser: false, 
+    }
   })
 
   if (error) {
-    return { error: error.message }
+    return { error: error.message, success: false, message: '' }
+  }
+
+  return { success: true, message: 'Check your email for the code!', error: '' }
+}
+
+export async function verifyOtp(previousState: any, formData: FormData) {
+  const supabase = await createClient()
+
+  const email = formData.get('email') as string
+  const token = formData.get('token') as string
+
+  const { error } = await supabase.auth.verifyOtp({
+    email,
+    token,
+    type: 'email',
+  })
+
+  if (error) {
+    return { error: error.message, success: false }
   }
 
   revalidatePath('/dashboard', 'layout')
